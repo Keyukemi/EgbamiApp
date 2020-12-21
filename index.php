3ef920d89abdc0bf379b2caf88908faa95f44860
Keyukemi <keyukemi.ku@gmail.com>
2020-12-21 13:03:29 -0800

    Landing-Page

--- index.php
+++ index.php
@@ -1,11 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EgbamiApp</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href= "https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-brands.min.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-brands.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-regular.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-regular.min.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-solid.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fa-solid.min.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fontawesome-all.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/fonts_awesome/fontawesome.min.css">
+    <script src="https://use.fontawesome.com/61b09a571b.js"></script>
 </head>
 <body>
-    
+<header>
+        <div>
+            <nav class="transparent">
+                <div class="container">
+                    <div class="nav-wrapper">
+                        <a href="#" class="sidenav-trigger" data-target="links"><i class="material-icons">menu</i></a>
+                        <ul class="hide-on-med-and-down right">
+                            <li class="active "><a href="#">Home</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                            <li></li>
+                            <li><a href="#vendorForm" class=" blue waves-effect waves-light btn"> Register a service</a></li> 
+                            <li><a href="#usersForm" class=" blue waves-effect waves-light btn"> Sign Up as a User </a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="sidenav" id="links">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#reviews">Testimonials</a></li>
+                            <li><a href="#">Blog</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </br> </br> </br></br> </br></br> </br></br> </br></br></br></br></br></br></br></br></br>
+        <div class="container center-align"><h3>Egbami is here to help you connect with the best artisans anywhere and anytime</h3></div>
+        </br> </br></br></br> 
+            <div class="container" id="searchbox" >
+                <input  class= "input-field" type="search" placeholder="Who are you looking for?" id="search-text-input">
+                <span><a><i class="fa fa-search"></i></a></span>
+
+            </div>
+    </header>
+    <section>
+        <div class="container" id="services section">
+            <div class="container" id="serviceTitle"><h2><a href="#">Services</a></h2></div>
+            <div class="row">
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/barber.jpg" alt="barber" width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Barber</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/saloon.jpg" alt="hairdresser"width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Hair Stylist</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/tailor.jpg" alt="tailor" width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Tailor</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/mechanic.png" alt="mechanic" width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Car mechanic</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/painters.png" alt="painter"width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Painter</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/plumber.png" alt="plumber" width="350" height="315">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title"><a href="#">Plumber</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12 m4 l4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="assets/images/gym instructor.jpg" alt="workout instructor" width="350" height="315">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title"><a href="#">Gym instructor</a></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4 l4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="assets/images/shop grocceries.jpg" alt="Buy growceries"width="350" height="315">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title"><a href="#"> Grocery Shopping</a></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4 l4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="assets/images/home chef.jpg" alt="chef" width="350" height="315">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title"><a href="#">Chef</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+         
+    <footer class="page-footer">
+        <div class="container center-align">
+            <ul>
+                <li>
+                    <a><i class="fa fa-facebook-square"></i></a>
+                    <a><i class="fa fa-instagram"></i></a>
+                    <a><i class="fa fa-twitter-square"></i></a>
+                    <a><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div class="container center-align">
+            <div class="row">
+                    <a class="white-text">About Us    </a>     <a class="white-text">Blog      </a>   <a class="white-text">Contact Us</a>
+            </div>
+        </div>
+        <div class="footer-copyright blue-text">
+            <div class="container">
+                <div class="container center-align">&copy; 2020 Egbami</div></br>
+                <div class="container center-align">Made with <i class="material-icons" id="love icon">favorite</i> by Project10-LAW</div>
+                <div class="container center-align">Pearls Africa Foundation</div>  
+            </div>
+        </div>
+            
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script>
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+            $('.materialboxed').materialbox();
+        })
+        </script>
 </body>
 </html>
