d4589050c90d567b0ccdb22fab76f3f5463968ca
Keyukemi <keyukemi.ku@gmail.com>
2020-12-23 08:45:08 -0800

    Place order page edit

--- order.php
+++ order.php
@@ -1,45 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/materialize/css/materialize.min.css">
-    <link rel="stylesheet" href="assets/css/materialize/css/font/material-icons.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <div class="center-align waves-lighten-2 green-text z-depth-3"><h3>Order Up!</h3</div><br>
-        
-    <div>
+<?php include('include/header.php');?> 
+    
+<section>
+<div class="container">
+        <h3 class="center-align">Place Your Order</h3><br>
         <form action="#" method="post"><br>
             <input type="text" name="name" placeholder="Full name" required/><br>
             <input type="text" name="number" placeholder="Phone Number" required/><br>
             <input type="text" name="email" placeholder="Email" required/><br>
            
-
-            <select>
-                    <option>Select Services Needed</option>
-                    <option>Auto Mechanics</option>
-                    <option>Beauty Services</option>
-                    <option>Carwash</option>
-                    <option>Dry Cleaning</option>
-                    <option>Fashion designer</option>
-                    <option>Photography</option>
-                    <option>Plumbing</option>
-                    <option>Others</option>
-            </select>
-         </div>
-         <input type="text" name="name" placeholder="What's the problem?"required/><br>
-         <input type="text" name="number" placeholder="How much is your budget?"required/><br>
-         <input type="text" name="email" placeholder="current address"required/><br>
-         <input type="text" name="number" placeholder="City"required/><br>
-         <input type="text" name="email" placeholder="State"required/><br>
-         <a href="#"><H4> <div class="center-align btn waves-effect waves-lighten-2 white-text z-depth-3 hoverable"><input type="submit" value="PROCEED TO BOOK"></div> </H4></a>
-
-     </form> 
+            <!-- <label for="services">Choose Service to offer: </label>
+            <select name="service" id="services">
+                <option value="barber">Barber</option>
+                <option value="hairStylist">Hair Stylist</option>
+                <option value="tailor">Tailor</option>
+                <option value="laundry">Laundry Services</option>
+                <option value="mechanic">Mechanic</option>
+                <option value="painter">Painter</option>
+                <option value="plumber">Plumber</option>
+                <option value="gymInstructor">Gym Instructor</option>
+                <option value="chef">Personal Chef</option>
+                <option value="shopper">Personal Shopper</option>
+                <option value="masseuse">Masseuse</option>
+                <option value="photography">Photographer</option>
+                <option value="makeup">Makeup Artiste</option>
+                   
+            </select> -->
+            <input type="text" name="name" placeholder="What's the problem?"required/><br>
+            <input type="text" name="number" placeholder="How much is your budget?"required/><br>
+            <input type="text" name="email" placeholder="current address"required/><br>
+            <input type="text" name="number" placeholder="City"required/><br>
+            <input type="text" name="email" placeholder="State"required/><br>
+         
+            <div class="center-align">
+                <button class="center-align hoverable black waves-effect waves-light btn z-depth-0 white-text">Submit</button>
+            </div>
+        </form> 
         </div>
     </div>
-</body>
-</html>
+</section>
+
+<?php include('include/footer.php');?> 
