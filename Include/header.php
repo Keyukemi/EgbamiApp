11f81aa0820a9bfb405ede0a5a79b33814ad79d8
Keyukemi <keyukemi.ku@gmail.com>
2020-12-23 03:39:21 -0800

    added header and footer

---
+++ Include/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+  <link rel="stylesheet" href="./assets/css/index.css">
+
+  <title>Egbami</title>
+
+
+<body>
+<header class="header-img">
+
+<div class="navbar-fixed">
+        <nav class="nav-wrapper transparent">
+            <div class="container">
+                <a href="index.php" class="brand-logo">
+                    <img src="assets/images/EgbamiLogo.png" alt="" width="100" height="110"></a>
+                <a href="#" class="sidenav-trigger" data-target="mobile-menu">
+                    <i class="material-icons">menu</i>
+                </a>
+
+                <ul class="right hide-on-med-and-down">
+                <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li></li>
+                    <li><a href="login.php" class=" black waves-effect waves-light btn z-depth-0"> Login</a></li> 
+                    <li><a href="signup.php" class=" black waves-effect waves-light btn z-depth-0"> Sign Up</a></li>
+                    <li><a href="#" class="btn-floating black darken-2 z-depth-0">
+                        <i class="material-icons">notifications</i>
+                    </a></li>
+                </ul>
+
+                <ul class="sidenav" id="mobile-menu">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a class="sidenave-close" href="#">Blog</a></li>
+                    <li><a class="sidenave-close" href="#">Testimonial</a></li> 
+                    <li><a class="sidenave-close" href="login.php">Login</a></li>
+                    <li><a class="sidenave-close" href="signup.php">Sign Up</a></li>
+                </ul>
+            </div>     
+        </nav> 
+    </header>
+
